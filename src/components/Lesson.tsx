d1f427ec9ba6137b008f4d3fcda84f3d155b9918
Vanessa Ferreira <vanessaf689@gmail.com>
2023-07-11 22:42:13 -0300

    configuration store

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,27 @@
+import { Video } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { play } from "../store/slices/player.slice";
+
+interface LessonProps {
+  title: string;
+  duration: string;
+}
+
+export function Lesson({ title, duration}: LessonProps) {
+  const dispatch = useDispatch()
+
+  function handlePlayLesson() {
+    dispatch(play("teste"))
+  }
+
+  return (
+    <button
+      onClick={handlePlayLesson}
+      className="flex items-center gap-3 text-sm text-zinc-400"
+    >
+      <Video className="w-4 h-4 text-zinc-500" />
+      <span>{title}</span>
+      <span className="ml-auto font-mono text-xs text-zinc-500">{duration}</span>
+    </button>
+  )
+}
