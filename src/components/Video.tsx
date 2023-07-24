1093cbee4bc336de7df757780940d17ff0947af4
Vanessa Ferreira <vanessaf689@gmail.com>
2023-07-24 14:45:08 -0300

    feat: loading

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,11 @@
+import { Loader } from "lucide-react";
 import ReactPlayer from "react-player";
-import { useDispatch } from "react-redux";
+import { useAppDispatch, useAppSelector } from "../store";
 import { next, useCurrentLesson } from "../store/slices/player.slice";
 
 export function Video() {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
 
   const { currentLesson } = useCurrentLesson()
 
@@ -11,22 +13,23 @@ export function Video() {
     dispatch(next())
   }
 
-  if (!currentLesson) {
-    return null
-  }
-
   return (
-    <div className="flex-1">
-      <div className="w-full bg-zinc-950 aspect-video">
+    <div className="w-full bg-zinc-950 aspect-video">
+      { isCourseLoading ? (
+        <div className="flex h-full items-center justify-center">
+          <Loader className="w-6 h-6 text-zinc-400 animate-spin" />
+        </div>
+      ) : (
         <ReactPlayer 
-          onEnded={handleNext}
-          width="100%" 
-          height="100%"
-          playing
-          controls 
-          url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
+        onEnded={handleNext}
+        width="100%" 
+        height="100%"
+        playing
+        controls 
+        url={`https://www.youtube.com/watch?v=${currentLesson?.id}`}
         />
-      </div>
+      )}
+      
     </div>
   )
 }
